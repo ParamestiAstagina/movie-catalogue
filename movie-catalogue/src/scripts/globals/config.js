9473524aa307618631e514888cd4e201addce198
Paramesti Astagina <astagina.paramesti@gmail.com>
2024-11-12 18:08:07 +0700

    Add files via upload

---
+++ movie-catalogue/src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: '6f520424e041aa6f70635379016fc8c4',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+};
+ 
+export default CONFIG;
